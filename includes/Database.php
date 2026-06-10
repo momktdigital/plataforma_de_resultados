1676d3ae855a76830bef42dd9486495e4198dc40
Matheus Oliveira <33221493+momktdigital@users.noreply.github.com>
2026-06-10 08:51:11 -0300

    Update Database.php

--- includes/Database.php
+++ includes/Database.php
@@ -29,7 +29,6 @@ class Database {
             $this->conn = new PDO($dsn, $this->username, $this->password, [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES   => false,
             ]);
         } catch (PDOException $e) {
             error_log('Falha na conexão com o banco: ' . $e->getMessage());
